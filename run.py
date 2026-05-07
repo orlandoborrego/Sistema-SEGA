4f42ede030f6db34cbc9b31013d3808631dff784
Orlando Borrego <orlando.borrego@fgjtam.gob.mx>
2026-05-07 13:54:47 -0600

    requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-# Cargar .env explícitamente
+
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 from app import create_app
@@ -9,4 +9,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)   
+    app.run(debug=False, host='0.0.0.0', port=5000)
--- test_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv('.env')
-
-print("DB_USER:", os.getenv('DB_USER'))
-print("DB_PASS:", os.getenv('DB_PASS'))
-print("DB_HOST:", os.getenv('DB_HOST'))
-print("DB_NAME:", os.getenv('DB_NAME'))
