157f6ba0631ea3ea4b50d2c9fcaa6bcbd5cab665
Orlando Borrego <orlando.borrego@fgjtam.gob.mx>
2026-06-05 13:21:59 -0600

    CAMBIO DE DISEÑO CSS

--- run.py
+++ run.py
@@ -3,6 +3,14 @@ import os
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
+print("=" * 50)
+print("DB_SERVER :", os.getenv("DB_SERVER"))
+print("DB_NAME   :", os.getenv("DB_NAME"))
+print("DB_USER   :", os.getenv("DB_USER"))
+print("DB_PASS   :", "********" if os.getenv("DB_PASS") else "NO DEFINIDO")
+print("HOST      :", os.getenv("HOST"))
+print("=" * 50)
+
 from app import create_app
 
 app = create_app()
